65add9962d9c430d798dd97548009b87702b52b7
alexUd01 <alexanderikpeama@gmail.com>
2022-10-17 11:39:38 +0100

    1

--- test/_write.c
+++ test/_write.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
+/**
+ * _write - a function that prints characters to stdout
+ * Return: returns the number of bytes printed (on success)
+ *         returns -1 (on error)
+ */
 int _write(char c)
 {
-	return(write(1, &c, 1);
+	return(write(1, &c, 1));
 }
