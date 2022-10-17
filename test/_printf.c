b232d1c0442116a84df95cf270f6d8b8262d7f46
alexUd01 <alexanderikpeama@gmail.com>
2022-10-17 11:26:18 +0100

    Began Task 0

---
+++ test/_printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format
+ * @format: a character  string
+ * Return: returns the number of characters printed (excludint the null
+ * byte use to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	
+}
---
+++ test/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+
+
+#endif /* MAIN_H */
